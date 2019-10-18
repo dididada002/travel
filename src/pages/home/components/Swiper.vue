6ae4c57394febe6e3b1e4d0c4c872f6453973f68
jingteng <jingteng@zk-house.com>
2019-10-18 12:25:04 +0800

    轮播图

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" :not-next-tick="notNextTick" ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img
+          class="swiper-img"
+          :src="item.imgUrl"
+        />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination"
+      },
+      swiperList: [
+          {
+              id: '0001',
+              imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/3e/52e1824f1ace7202.jpg_750x200_ac90db3f.jpg'
+          },{
+              id: '0002',
+              imgUrl: 'http://img1.qunarzz.com/piao/fusion/1709/39/b98fdb3ee7478e02.jpg_750x200_be64b756.jpg'
+          }
+      ]
+    };
+  }
+};
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #ffffff;
+//   给swipper下面的滑动点设置样式，必须穿透出wrapper给swipper组件设置 
+}
+
+.wrapper {
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
