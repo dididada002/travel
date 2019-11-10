319f9b4a3eba74d1d854e8206b3197764ddee966
dididada002 <17610300902@163.com>
2019-11-10 16:40:22 +0800

    推荐组件

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img
+          class="item-img"
+          :src= "item.imgUrl"
+        />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeRecommend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          title: "标题1",
+          desc: "内容1"
+        },{
+            id: "002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          title: "标题2",
+          desc: "内容2"
+        },{
+            id: "003",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          title: "标题3",
+          desc: "内容3"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style scoped lang="stylus" scoped>
+@import '~styles/mixins.styl';
+
+.recommend-title {
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+  margin-top: 0.2rem;
+}
+
+.item {
+  overflow: hidden;
+  display: flex;
+  height: 1.9rem;
+
+  .item-img {
+    width: 1.7rem;
+    height: 1.7rem;
+    padding: 0.1rem;
+  }
+
+  .item-info {
+    flex: 1;
+    padding: 0.1rem;
+    min-width: 0;
+
+    .item-title {
+      line-height: 0.54rem;
+      font-size: 0.32rem;
+      ellipsis();
+    }
+
+    .item-desc {
+      line-height: 0.4rem;
+      color: #cccccc;
+      ellipsis();
+    }
+
+    .item-button {
+      line-height: 0.4rem;
+      margin-top: 0.16rem;
+      background: #ff9300;
+      padding: 0 0.2rem;
+      border-radius: 0.06rem;
+      color: #ffffff;
+    }
+  }
+}
+</style>
